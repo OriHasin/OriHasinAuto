e5d7631880d996e62706039ed1995c58c483d6d2
ziv <ziv04021999@gmail.com>
2020-10-04 21:10:04 +0300

    04\10\20

--- OriHasinAutomationTesting/Pages.py
+++ OriHasinAutomationTesting/Pages.py
@@ -13,6 +13,10 @@ class HomePage:
         self.driver=driver
     def CategoryIcon(self,category):
         self.driver.find_element_by_id(f"{category.lower()}Img").click()
+
+    def HomePageValidation(self):
+        return self.driver.find_elements_by_xpath("//div[@href='javascript:void(0)']")
+
     def WaitToHomepage(self):
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "headphonesImg")))
 
@@ -23,14 +27,32 @@ class CartPage:
     def __init__(self,driver):
         self.driver=driver
 
+    def ProductPriceInCart(self,index):
+        #index+=3
+        return self.driver.find_elements_by_xpath('//p [@class="price roboto-regular ng-binding"]')[index].text
+
+    def ProductNameInCart(self,index):
+        return self.driver.find_elements_by_xpath('//label [@class="roboto-regular productName ng-binding"]')[index].text
+
+    def ProductQtyInCart(self,index):
+        return self.driver.find_elements_by_xpath('//tbody/tr/td/label [@class="ng-binding"]')[index].text
+
     def EditButton(self,index): # לחיצה כל כפתור עריכה של מוצר ספציפי בעגלת הקניות
          self.driver.find_elements_by_css_selector("a[class='edit ng-scope']")[index].click()
 
     def ShoppingCartText(self): # מחזיר את הטקסט "Shopping Cart" בעגלת הקניות
         return self.driver.find_element_by_class_name("select  ng-binding").text
 
+
     def TotalPrice(self):
-        return self.driver.find_element_by_xpath("//span[@class='roboto-medium ng-binding'][3]").text
+        return self.driver.find_element_by_xpath('// *[ @ id = "shoppingCart"] / table / tfoot / tr[1] / td[2] / span[2]').text
+
+    def CheckoutButtonCartpage(self):
+        self.driver.find_element_by_xpath("//button[@class='roboto-medium tami uft-class ng-binding']").click()
+
+
+    def WaitToCartpage(self):
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '//h3[@class="roboto-regular center sticky fixedImportant ng-binding"]')))
 
 
 ###############################################################################################
@@ -56,6 +78,9 @@ class CartIcon:
         return self.driver.find_elements_by_css_selector("h3[class='ng-binding']")[index].text
     def RemoveInCartIcon(self,index):
         return self.driver.find_elements_by_css_selector("[class='removeProduct iconCss iconX']")[index].click()
+    def LengthListOfProducts(self):
+        return len(self.driver.find_elements_by_xpath('//img [@class="imageUrl"]'))
+
 
 
 ###############################################################################################
@@ -74,6 +99,9 @@ class CategoryPage:
 
     def ProductsInCategory(self):
         return len(self.driver.find_elements_by_css_selector("img[class='imgProduct']"))
+
+    def TabletsCategoryValidation(self):
+        return self.driver.find_elements_by_xpath("//h4[@href='javascript:void(0)']")
 
     def WaitToCategorypage(self):
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "imgProduct")))
@@ -109,6 +137,61 @@ class ProductPage:
 
     def WaitToProductpage(self):
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.NAME, "save_to_cart")))
+
+class OrderPaymentPage:
+
+    def __init__(self,driver):
+        self.driver=driver
+
+    def InsertUserExist(self,username,password):
+        self.driver.find_element_by_xpath('//input[@name="usernameInOrderPayment"]').send_keys(username)
+        self.driver.find_element_by_xpath('//input[@name="passwordInOrderPayment"]').send_keys(password)
+
+    def LogInButton(self):
+        self.driver.find_element_by_xpath('//button[@id="login_btnundefined"]').click()
+
+    def NextButton(self):
+        self.driver.find_element_by_xpath('//button[@class="a-button nextBtn marginTop75 ng-scope"]').click()
+
+    def EditButton(self):
+        self.driver.find_element_by_xpath('//label[@class="edit  ng-scope"]').click()
+
+    def PayNowButtonMasterCard(self):
+        self.driver.find_element_by_xpath('//button[@style="    margin-bottom: 5px;"]').click()
+
+    def OrderNumberGet(self):
+       return self.driver.find_element_by_xpath('//label[@id="orderNumberLabel"]').text
+
+    def WaitToOrderPaymentPage(self):
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//h5[@translate='ORDER_SUMMARY']")))
+
+    def WaitToShippingDetailsPage(self):
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '//label[@class="roboto-regular ng-binding selected"]')))
+
+    def WaitToPaymentMethodPage(self):
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[@class="paymentMethods"]')))
+
+    def WaitToOrderCompletePage(self):
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//div[@class="seccion borderRight"]')))
+
+class UserIcon:
+    def __init__(self,driver):
+        self.driver=driver
+
+    def UserOrdersEnter(self):
+        self.driver.find_element_by_xpath('//a[@id="menuUserLink"]').click()
+        self.driver.find_element_by_xpath('//a/div/label[@translate="My_Orders"]').click()
+
+    def OrderInList(self,ordernumber):
+        list1=self.driver.find_elements_by_css_selector("label[class='ng-binding']")
+        reversed(list1)
+        for i in range(len(list1)):
+            if list1[i].text == ordernumber:
+                return True
+        return False
+
+    def WaitToMyOrdersPage(self):
+        WebDriverWait(self.driver, 16).until(EC.presence_of_element_located((By.XPATH, '//label[@class="center ng-binding"]')))
 
 
 
--- OriHasinAutomationTesting/TestProject.py
+++ OriHasinAutomationTesting/TestProject.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest import TestCase
 from selenium import webdriver
-from OriHasinAutomationTesting.Pages import HomePage,CategoryPage,CartPage,CartIcon,ProductPage
+from OriHasinAutomationTesting.Pages import HomePage,CategoryPage,CartPage,CartIcon,ProductPage,OrderPaymentPage,UserIcon
 from random import randint
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -13,14 +13,16 @@ from time import sleep
 class AOSTests(TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path="C:/Users/User/Desktop/Ori Selenium/chromedriver.exe")
+        self.driver = webdriver.Chrome(executable_path="C:\selenium_driver\chromedriver.exe")
         self.driver.get("http://advantageonlineshopping.com/#/")
         self.ListOfCategories=['headphones','mice','tablets','laptops','speakers']
-        self.Hpage = HomePage(self.driver)
-        self.Cpage = CategoryPage(self.driver)
-        self.Ppage = ProductPage(self.driver)
-        self.Cicon=CartIcon(self.driver)
-        self.Cartpage=CartPage(self.driver)
+        self.hpage = HomePage(self.driver)
+        self.cpage = CategoryPage(self.driver)
+        self.ppage = ProductPage(self.driver)
+        self.cicon=CartIcon(self.driver)
+        self.cartpage=CartPage(self.driver)
+        self.orderpayment=OrderPaymentPage(self.driver)
+        self.usericon=UserIcon(self.driver)
         self.driver.maximize_window()
 
     def tearDown(self) :
@@ -28,9 +30,9 @@ class AOSTests(TestCase):
         self.driver.close()
 
     def test1(self):
-        hpage1=HomePage(self.driver)
-        cpage1=CategoryPage(self.driver)
-        ppage1=ProductPage(self.driver)
+        self.hpage1=HomePage(self.driver)
+        self.cpage1=CategoryPage(self.driver)
+        self.ppage1=ProductPage(self.driver)
         cartIcon1=CartIcon(self.driver)
         sumquantity=0
         for i in range(2):
@@ -38,18 +40,17 @@ class AOSTests(TestCase):
             randomproduct=randint(0,2)
             randomquantity=randint(1,5)
             sumquantity += randomquantity
-            hpage1.WaitToHomepage()
-            hpage1.CategoryIcon(self.ListOfCategories[randomcategory])
-            cpage1.WaitToCategorypage()
-            cpage1.GetProduct(randomproduct)
-            ppage1.WaitToProductpage()
-            ppage1.PlusQuantity(randomquantity)
-            ppage1.AddToCartButton()
-            ppage1.BackToCategory()
-            cpage1.BackToHomepage()
+            self.hpage1.WaitToHomepage()
+            self.hpage1.CategoryIcon(self.ListOfCategories[randomcategory])
+            self.cpage1.WaitToCategorypage()
+            self.cpage1.GetProduct(randomproduct)
+            self.ppage1.WaitToProductpage()
+            self.ppage1.PlusQuantity(randomquantity)
+            self.ppage1.AddToCartButton()
+            self.ppage1.BackToCategory()
+            self.cpage1.BackToHomepage()
 
-        hpage1.WaitToHomepage()
-        sleep(1)
+        self.hpage1.WaitToHomepage()
         self.assertEqual(int(cartIcon1.NumberOfProducts()),sumquantity+2 or int(cartIcon1.NumberOfProducts()),sumquantity+1 )
 
     def test_Exercise2(self):
@@ -85,6 +86,122 @@ class AOSTests(TestCase):
             self.assertEqual(ListOfProducts[i][2], self.Cicon.QtyInCartIcon(2-i))
             self.assertEqual(ListOfProducts[i][3], self.Cicon.PriceInCartIcon(2-i))
             #pointer-=1
+
+
+    def test_Exercise3(self):
+        counter1=0
+        for i in range(2):
+            counter1+=1
+            randomcategory=randint(0,4)
+            randomproduct=randint(0,2)
+            randomquantity=randint(1,5)
+            self.hpage.WaitToHomepage()
+            self.hpage.CategoryIcon(self.ListOfCategories[randomcategory])
+            self.cpage.WaitToCategorypage()
+            self.cpage.GetProduct(randomproduct)
+            self.ppage.WaitToProductpage()
+            self.ppage.PlusQuantity(randomquantity)
+            self.ppage.AddToCartButton()
+            self.ppage.BackToCategory()
+            self.cpage.BackToHomepage()
+        if self.cicon.LengthListOfProducts()==counter1:
+            self.cicon.RemoveInCartIcon(1)
+        print(self.cicon.LengthListOfProducts(),counter1)
+        self.assertTrue(counter1==self.cicon.LengthListOfProducts()+1)
+
+    def test_Exercise5(self):
+        listqty=[]
+        listcat=[]
+        randomquantity = randint(1,5)
+        randomcategory = randint(0,4)
+        for i in range(3):
+            bool1=True
+            while bool1:
+                if randomquantity in listqty:
+                    randomquantity = randint(1,5)
+                else:
+                    listqty.append(randomquantity)
+                    bool1=False
+            bool2=True
+            while bool2:
+                if randomcategory in listcat:
+                    randomcategory = randint(0,4)
+                else:
+                    listcat.append(randomcategory)
+                    bool2=False
+            randomproduct = randint(0, 2)
+            self.hpage.WaitToHomepage()
+            self.hpage.CategoryIcon(self.ListOfCategories[randomcategory])
+            self.cpage.WaitToCategorypage()
+            self.cpage.GetProduct(randomproduct)
+            self.ppage.WaitToProductpage()
+            self.ppage.PlusQuantity(randomquantity)
+            self.ppage.AddToCartButton()
+            self.ppage.BackToCategory()
+            self.cpage.BackToHomepage()
+            randomquantity = randint(1,5)
+            randomcategory = randint(0,4)
+        self.cicon.CartIcon().click()
+        self.cartpage.WaitToCartpage()
+        sumprices=0
+        for i in range(0,3,1):
+            tmpprice=self.cartpage.ProductPriceInCart(i)
+            tmpprice=tmpprice.replace(",","")
+            tmpprice=tmpprice[1:]
+            sumprices+=float(tmpprice)
+            print(sumprices)
+            print("The Product Name Is:" + self.cartpage.ProductNameInCart(i) + "The QTY Is:" + self.cartpage.ProductQtyInCart(i) + "The Price Is:" + self.cartpage.ProductPriceInCart(i))
+        totalprice=self.cartpage.TotalPrice()
+        totalprice=totalprice.replace(",","")
+        totalprice=totalprice[1:]
+        self.assertEqual(float(totalprice),round(sumprices,2))
+
+    def test_Exercise7(self):
+        self.hpage.WaitToHomepage() #wait to homepage
+        self.hpage.CategoryIcon("tablets") #enter category tablets
+        self.cpage.WaitToCategorypage() #wait to category page
+        self.cpage.GetProduct(randint(0,2)) #choose random product
+        self.ppage.WaitToProductpage() #wait to product page
+        self.ppage.PlusQuantity(randint(1,9)) #add random quantity
+        self.ppage.AddToCartButton() #add to cart
+        self.ppage.BackToCategory() #back to tablets page
+        self.cpage.WaitToCategorypage() #wait
+        self.assertEqual(len(self.cpage.TabletsCategoryValidation()),4) #make sure it is the tablets page
+        self.cpage.BackToHomepage() #back to homepage
+        self.hpage.WaitToHomepage() #wait
+        self.assertEqual(len(self.hpage.HomePageValidation()),5) #make sure it is the home page
+
+    def test_Exercise9(self):
+        self.hpage.WaitToHomepage()  # wait to homepage
+        self.hpage.CategoryIcon("tablets")  # enter category tablets
+        self.cpage.WaitToCategorypage()  # wait to category page
+        self.cpage.GetProduct(randint(0, 2))  # choose random product
+        self.ppage.WaitToProductpage()  # wait to product page
+        self.ppage.PlusQuantity(randint(1, 9))  # add random quantity
+        self.ppage.AddToCartButton()  # add to cart
+        self.cicon.CartIcon().click()
+        self.cartpage.WaitToCartpage()
+        self.cartpage.CheckoutButtonCartpage()
+        self.orderpayment.WaitToOrderPaymentPage()
+        self.orderpayment.InsertUserExist("zivush","Zzziiivvv4")
+        self.orderpayment.LogInButton()
+        self.orderpayment.WaitToShippingDetailsPage()
+        sleep(3)
+        self.orderpayment.NextButton()
+        self.orderpayment.WaitToPaymentMethodPage()
+        self.orderpayment.PayNowButton()
+        self.orderpayment.WaitToOrderCompletePage()
+        ordernumber = self.orderpayment.OrderNumberGet()
+        print("hey",ordernumber)
+        sleep(3)
+        self.assertEqual(self.cicon.LengthListOfProducts(),0)
+        self.usericon.UserOrdersEnter()
+        self.usericon.WaitToMyOrdersPage()
+        self.assertTrue(self.usericon.OrderInList(ordernumber))
+
+
+
+
 
 
 
